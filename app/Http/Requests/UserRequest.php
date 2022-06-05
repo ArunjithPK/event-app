27d6efd3be0b204ee7c509d4dc09a8f7527eaf99
Arunjith <arunjith.pk@secture.ca>
2022-06-05 12:54:59 +0530

    password  validation

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -20,7 +20,7 @@ class UserRequest extends FormRequest
             'email' => "required|email|unique:users",
             'gender'=>"required",
             'dob' => "date|date_format:Y-m-d|before_or_equal:".Carbon::now()->subYears(18)->format('Y-m-d'),
-            'password' => 'required|confirmed',
+            'password' => 'required|min:6|confirmed',
         ];
     }
 
